d7a8665780ffd409cbcd2c53b9637772e11da27f
MaegIins <62724143+MaegIins@users.noreply.github.com>
2021-06-05 17:56:39 +0200

    Constructeur du labyrinthe

--- projet_zeldiablo/Labyrinthe.java
+++ projet_zeldiablo/Labyrinthe.java
@@ -5,7 +5,36 @@ public class Labyrinthe {
     private Entree e;
 
     public Labyrinthe() {
+        //x = chemin
+        //o = obstacle
+        String lab =
+                "xoooooooooooooo" +
+                "xxoxxxooxxxxoxx" +
+                "oxxxoxooxooxoxo" +
+                "oooooxoxxoxxoxo" +
+                "oxxoxxoxxoxooxx" +
+                "oxooxooxoxxooox" +
+                "oxoxxooxoxoooox" +
+                "oxxxoxoxoxxxxox" +
+                "oooxoxxxxoooxxx" +
+                "xxoxxoooxoooooo" +
+                "oxooxoxxxxxxxox" +
+                "oxxoxxxoooooxox" +
+                "ooxooooxxxxxxox" +
+                "ooxooxxxooxooox" +
+                "ooxxxxooooxxxxx";
 
+        for (int i = 0 ; i < lab.length(); i++){
+            String c = lab.charAt(i);
+            if (c == "x"){
+                Chemin c1 = new Chemin();
+                cases.add(c1);
+            }
+            if (c == "o"){
+                Mur m1 = new Mur();
+                cases.add(m1);
+            }
+        }
     }
 
     public boolean creerEntree(int x, int y) {
