900b59a2a65dfa547ad3b860cb8acba2a2d31d61
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2021-06-06 00:31:54 +0200

    Review all Labyrinthe

--- projet_zeldiablo/Labyrinthe.java
+++ projet_zeldiablo/Labyrinthe.java
@@ -1,90 +1,149 @@
-import java.util.*;
-
+/**
+ * Classe modelisant un labyrinthe
+ *
+ * @author AGJMX
+ */
 public class Labyrinthe {
-    private Case[][] cases;
-    private Joueur joueur;
-    private Entree entree;
 
+    /**
+     * Tableau de cases
+     */
+    private final Case[][] cases;
+
+    /**
+     * Joueur, personnage principal
+     */
+    private final Joueur joueur;
+
+    /**
+     * Porte sur laquelle le joueur apparait
+     */
+    private Porte entree;
+
+    /**
+     * Point de sortie pour le joueur
+     */
+    private Porte sortie;
+
+    /**
+     * Constructeur public par defaut
+     */
     public Labyrinthe() {
-
+        cases = new Case[15][15];
         //x = chemin
         //o = obstacle
         String lab =
                 "xoooooooooooooo" +
-                "xxoxxxooxxxxoxx" +
-                "oxxxoxooxooxoxo" +
-                "oooooxoxxoxxoxo" +
-                "oxxoxxoxxoxooxx" +
-                "oxooxooxoxxooox" +
-                "oxoxxooxoxoooox" +
-                "oxxxoxoxoxxxxox" +
-                "oooxoxxxxoooxxx" +
-                "xxoxxoooxoooooo" +
-                "oxooxoxxxxxxxox" +
-                "oxxoxxxoooooxox" +
-                "ooxooooxxxxxxox" +
-                "ooxooxxxooxooox" +
-                "ooxxxxooooxxxex";
-
-        for (int i = 0 ; i < 15; i++){
-            for(int j = 0; j < 15; j++){
-                int cursor = 0;
-                char c = lab.charAt(cursor);
-                if (c == 'x'){
-                    Chemin c1 = new Chemin(i, j);
-                    cases[i][j]=c1;
-                }
-                if (c == 'o'){
-                    Mur m1 = new Mur(i, j);
-                    cases[i][j]=m1;
-                }
-                if (c == 'e'){
-                    entree = new Entree(i, j);
-                    cases[i][j]=entree;
+                        "xxoxxxooxxxeoxx" +
+                        "oxxxoxooxooxoxo" +
+                        "oooooxoxxoxxoxo" +
+                        "oxxoxxoxxoxooxx" +
+                        "oxooxooxoxxooox" +
+                        "oxoxxooxoxoooox" +
+                        "oxxxoxoxoxxxxox" +
+                        "oooxoxxxxoooxxx" +
+                        "xxoxxoooxoooooo" +
+                        "oxooxoxxxxxxxox" +
+                        "oxxoxxxoooooxox" +
+                        "ooxooooxxxxxxox" +
+                        "ooxooxxxooxooox" +
+                        "ooxxxxooooxxxex";
+        int cursor = 0;
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                switch (lab.charAt(cursor)) {
+                    case 'x':
+                        Case c1 = new Chemin(i, j);
+                        cases[i][j] = c1;
+                        break;
+                    case 'o':
+                        Case m1 = new Mur(i, j);
+                        cases[i][j] = m1;
+                        break;
+                    case 'e':
+                        Porte p = new Porte(i, j);
+                        cases[i][j] = p;
+                        if (this.entree == null) {
+                            this.entree = p;
+                        } else {
+                            sortie = p;
+                        }
+                        break;
                 }
                 cursor++;
             }
-
         }
-        this.joueur = new Joueur(entree);
+        this.joueur = new Joueur(this, entree);
     }
 
-    public boolean peutBouger(Personnage p, Direction d){
-        boolean res = false;
-        switch (d) {
-            case NORTH -> {
-                Case nord = cases[p.getCase().x][p.getCase().y - 1];
-                if (nord.x >= 0) {
-                    if (nord instanceof Chemin) {
-                        res = true;
-                    }
-                }
-            }
-            case SOUTH -> {
-                Case sud = cases[p.getCase().x][p.getCase().y + 1];;
-                if (sud.x <= 14) {
-                    if (sud instanceof Chemin) {
-                        res = true;
-                    }
-                }
-            }
-            case EAST -> {
-                Case est = cases[p.getCase().x + 1][p.getCase().y];
-                if (est.y <= 14) {
-                    if (est instanceof Chemin) {
-                        res = true;
-                    }
-                }
-            }
-            case WEST -> {
-                Case ouest = cases[p.getCase().x - 1][p.getCase().y];
-                if (ouest.y >= 0) {
-                    if (ouest instanceof Chemin) {
-                        res = true;
-                    }
-                }
-            }
+    /**
+     * Methode de test pour savoir si un Personnage peut bouger
+     * On checrche la case sur laquelle il veut aller puis on verifie si le personnage peut aller dessus
+     *
+     * @param p   personnage a tester
+     * @param dir direction souhaitee
+     * @return booleen, a vrai s'il peut bouger
+     */
+    private boolean peutBouger(Personnage p, Direction dir) {
+        Case destination = null;
+        try {
+            destination = getDestination(p, dir);
+        } catch (IndexOutOfBoundsException ignored) {
         }
-        return res;
+        if (destination != null) {
+            return destination.peutTraverser(p);
+        }
+        return false;
+    }
+
+    public void jouer() {
+        //TODO METHODE PRINCIPALE
+    }
+
+    /**
+     * Methode pour deplacer un Personnage
+     * Appelle d'abord peutBouger
+     *
+     * @param p Personnage a deplacer
+     * @param d Direction voulue
+     * @return booleen, a vrai si le personnage a bouge
+     */
+    public boolean deplacerJoueur(Personnage p, Direction d) {
+        if (peutBouger(p, d)) {
+            p.setPosition(getDestination(p, d));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Methode getDestination, utilisee pour trouver la case avec une direction
+     *
+     * @param p personnage
+     * @param d direction voulue
+     * @return Case correspondant a la destination
+     */
+    private Case getDestination(Personnage p, Direction d) {
+        Case actuel = p.getCase();
+        Case destination = null;
+        switch (d) {
+            case NORTH:
+                destination = cases[actuel.x][actuel.y - 1];
+                break;
+            case SOUTH:
+                destination = cases[actuel.x][actuel.y + 1];
+                break;
+            case EAST:
+                destination = cases[actuel.x + 1][actuel.y];
+                break;
+            case WEST:
+                destination = cases[actuel.x - 1][actuel.y];
+                break;
+        }
+        return destination;
+    }
+
+    public Joueur getJoueur() {
+        return joueur;
     }
 }
