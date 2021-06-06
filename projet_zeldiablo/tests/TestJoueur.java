e0984dfe6b029d6dc73d5e3260f1207c3820c5ae
MaegIins <62724143+MaegIins@users.noreply.github.com>
2021-06-06 18:11:41 +0200

    test01 dans TestJoueur

--- projet_zeldiablo/tests/TestJoueur.java
+++ projet_zeldiablo/tests/TestJoueur.java
@@ -1,9 +1,31 @@
 package tests;
 
+import jeu.Labyrinthe;
+import jeu.cases.Case;
+import jeu.cases.Chemin;
+import jeu.cases.Porte;
+import jeu.entites.Joueur;
+import jeu.entites.Personnage;
+import jeu.utils.Direction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 /**
  * Classe pour les tests de Joueur
  *
  * @author AGJMX
  */
 public class TestJoueur {
+
+    @Test
+    public void test01_Joueur_ConstructeurPoisition_OK(){
+        Labyrinthe l = new Labyrinthe();
+        Porte p = new Porte(5,8);
+        Joueur j = new Joueur(l,p);
+        assertEquals("le joueur doit etre en 5,8", j.getCase(), p);
+    }
+
+
+
 }
--- projet_zeldiablo/tests/TestLabyrinthe.java
+++ projet_zeldiablo/tests/TestLabyrinthe.java
@@ -1,9 +1,26 @@
 package tests;
 
+import jeu.Labyrinthe;
+import jeu.cases.Case;
+import jeu.cases.Porte;
+import jeu.entites.Joueur;
+import jeu.entites.Personnage;
+import jeu.utils.Direction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Classe pour les tests de Labyrinthe
  *
  * @author AGJMX
  */
 public class TestLabyrinthe {
+
+
+
+
+
+
 }
