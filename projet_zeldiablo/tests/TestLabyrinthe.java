8deddd2a83c834f31ad8bef3ec766460d3e4d017
Androymm <antonin.froment@hotmail.com>
2021-06-07 00:51:34 +0200

    Re Test Labyrinthe

--- projet_zeldiablo/tests/TestLabyrinthe.java
+++ projet_zeldiablo/tests/TestLabyrinthe.java
@@ -21,9 +21,12 @@ public class TestLabyrinthe {
   @Test
   public void test_creerLabyrinthe(){
       Labyrinthe l = new Labyrinthe();
-      assertEquals("cette case est cense etre une porte", );
-      assertEquals("cette case est cense etre un mur", );
-      assertEquals("cette case est cense etre un chemin", );
+      String c1 = l.getCase(14,13);
+      String c2 = l.getCase(0,1);
+      String c3 = l.getCase(0,0);
+      assertEquals("cette case est cense etre une porte","E", c1);
+      assertEquals("cette case est cense etre un mur","\u220E", c2);
+      assertEquals("cette case est cense etre un chemin","X", c3);
   }
 
   @Test
@@ -36,14 +39,6 @@ public class TestLabyrinthe {
       assertEquals("mauvaise position en X",1,j.getCase().x);
       assertEquals("mauvaise position en Y",1,j.getCase().y);
   }
-
-  @Test
-  public void test_getDestination(){
-      Labyrinthe l = new Labyrinthe();
-      Porte e = new Porte(0,0);
-      Joueur j = new Joueur(l,e);
-  }
-
 
 
 
