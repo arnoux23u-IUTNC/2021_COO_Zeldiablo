deef42c1df8a9c98f588674188c81d3de2c573d1
Androymm <antonin.froment@hotmail.com>
2021-06-07 00:31:25 +0200

    Test Labyrinthe

--- projet_zeldiablo/tests/TestLabyrinthe.java
+++ projet_zeldiablo/tests/TestLabyrinthe.java
@@ -18,7 +18,31 @@ import static org.junit.Assert.*;
  */
 public class TestLabyrinthe {
 
+  @Test
+  public void test_creerLabyrinthe(){
+      Labyrinthe l = new Labyrinthe();
+      assertEquals("cette case est cense etre une porte", );
+      assertEquals("cette case est cense etre un mur", );
+      assertEquals("cette case est cense etre un chemin", );
+  }
 
+  @Test
+  public void test_deplacerJoueur(){
+      Labyrinthe l = new Labyrinthe();
+      Porte e = new Porte(0,0);
+      Joueur j = new Joueur(l,e);
+      l.deplacerJoueur(j,Direction.SOUTH);
+      l.deplacerJoueur(j,Direction.EAST);
+      assertEquals("mauvaise position en X",1,j.getCase().x);
+      assertEquals("mauvaise position en Y",1,j.getCase().y);
+  }
+
+  @Test
+  public void test_getDestination(){
+      Labyrinthe l = new Labyrinthe();
+      Porte e = new Porte(0,0);
+      Joueur j = new Joueur(l,e);
+  }
 
 
 
