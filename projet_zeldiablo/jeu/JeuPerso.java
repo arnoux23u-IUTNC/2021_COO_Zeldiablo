f4825ec0f14c5b8137307edc969a8961ebc88893
GermonvilleBellet Alexandre <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2021-06-07 11:42:21 +0200

    creation interface jeu et modif jeuperso

---
+++ projet_zeldiablo/jeu/JeuPerso.java
@@ -0,0 +1,28 @@
+package jeu;
+
+import jeu.entites.Joueur;
+import jeu.utils.Direction;
+import moteurgraphique.Jeu;
+
+public class JeuPerso implements Jeu {
+
+    private Labyrinthe l;
+    private Joueur pj;
+    private boolean partieEnCours;
+
+    public JeuPerso(){
+        this.l = new Labyrinthe();
+        this.pj = l.getJoueur();
+    }
+
+
+    @Override
+    public void evoluer(Direction d) {
+
+    }
+
+    @Override
+    public boolean etreFini() {
+        return !partieEnCours;
+    }
+}
